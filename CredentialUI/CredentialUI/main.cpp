049cf560eeb90082b1fad5c20661aaf5db7e5163
msafwankarim <66724151+msafwankarim@users.noreply.github.com>
2021-03-29 19:39:17 +0500

    Update main.cpp

--- CredentialUI/CredentialUI/main.cpp
+++ CredentialUI/CredentialUI/main.cpp
@@ -1,131 +1,20 @@
+
+/*
+#===========================================================================================#
+*	Author: 		Safwan																    *
+*	Date:			Monday, 22 March 2021  5:23:34 PM									    *
+*	Brief:			Clones windows Administrator Password prompt using WinCred API		    *
+*																						    *
+#===========================================================================================#
+*/
+
+
 #include<Windows.h>     //Global functions and types of Win32
 #include<wincred.h>     //Required for Credential UI functions
 #include <iostream>     //Required for console io
 
 #pragma comment(lib,"Credui.lib")       //Dynamic linking with Credui.lib
 
-
-/*
-#===========================================================================================#
-*	Author: 		Safwan																    *
-*	Date:			Monday, ‎22 ‎March ‎2021, ‏‎5:23:34 PM									*
-*	Brief:			Clones windows Administrator Password prompt using WinCred API		    *
-*																						    *
-#===========================================================================================#
-*/
-
-//int main() {
-    /*
-    CREDUI_INFO cui;
-    TCHAR pszName[CREDUI_MAX_USERNAME_LENGTH + 1];
-    TCHAR pszPwd[CREDUI_MAX_PASSWORD_LENGTH + 1];
-    BOOL fSave;
-    DWORD dwErr;
-
-    cui.cbSize = sizeof(CREDUI_INFO);
-    cui.hwndParent = NULL;
-    //  Ensure that MessageText and CaptionText identify what credentials
-    //  to use and which application requires them.
-    cui.pszMessageText = TEXT("Enter administrator account information");
-    cui.pszCaptionText = TEXT("Administrator Access Required");
-    cui.hbmBanner = NULL;
-    fSave = FALSE;
-    SecureZeroMemory(pszName, sizeof(pszName));
-    SecureZeroMemory(pszPwd, sizeof(pszPwd));
-    dwErr = CredUIPromptForCredentials(
-        &cui,                         // CREDUI_INFO structure
-        TEXT("Host"),            // Target for credentials
-                                      //   (usually a server)
-        NULL,                         // Reserved
-        0,                            // Reason
-        pszName,                      // User name
-        CREDUI_MAX_USERNAME_LENGTH + 1, // Max number of char for user name
-        pszPwd,                       // Password
-        CREDUI_MAX_PASSWORD_LENGTH + 1, // Max number of char for password
-        &fSave,                       // State of save check box
-        CREDUI_FLAGS_GENERIC_CREDENTIALS |  // flags
-        CREDUI_FLAGS_ALWAYS_SHOW_UI |
-        CREDUI_FLAGS_DO_NOT_PERSIST);
-
-    if (!dwErr)
-    {
-        FILE* stream;
-        _wfreopen_s(&stream,L"log.txt", L"w", stdout);
-        //  Put code that uses the credentials here.
-        std::wcout << L"Username: " << pszName << std::endl << L"Password: " << pszPwd << std::endl;
-        //  When you have finished using the credentials,
-        //  erase them from memory.
-        SecureZeroMemory(pszName, sizeof(pszName));
-        SecureZeroMemory(pszPwd, sizeof(pszPwd));
-    }
-    system("type log.txt");
-    */
-    //std::wstring strDbPath = L"...";
-    //std::wstring strMsg = L"Enter your password to connect to: " + strDbPath;
-    //wchar_t init[] = L"Empty";
-    //wchar_t* pszUserName = init;
-    //wchar_t* pszPassword = init;
-    //wchar_t* pszDomainName = init;
-    //CREDUI_INFOW credui = {};
-    //credui.cbSize = sizeof(credui);
-    //credui.hwndParent = nullptr;
-    //credui.pszMessageText = strMsg.c_str();
-    //credui.pszCaptionText = L"Enter Network Password";
-    //credui.hbmBanner = nullptr;
-    ////SEC_WINNT_AUTH_IDENTITY authPackage;
-    //ULONG authPackage = 0;
-    //LPVOID outCredBuffer = nullptr;
-    //ULONG outCredSize = sizeof(outCredBuffer);
-    //BOOL save = false;
-    //DWORD dwFlags = CRED_PACK_GENERIC_CREDENTIALS;
-    //ULONG* pAuthBuffer = &authPackage;
-    //int cbAuthBuffer = sizeof(pAuthBuffer);
-    //DWORD pcchlMaxUserName = CREDUI_MAX_USERNAME_LENGTH + 1;
-    //
-    
-   
-
-  /*  int result = CredUIPromptForWindowsCredentialsW(
-        &credui,
-        0,
-        &authPackage,
-        nullptr,
-        NULL,
-        &outCredBuffer,
-        &outCredSize, 
-        &save, 1);
-    if (!result)
-    {
-        DWORD dwCredSize = outCredSize;
-        BOOL bresult = CredUnPackAuthenticationBuffer(
-            dwFlags,
-            outCredBuffer,
-            dwCredSize,
-            pszUserName,
-            &pcchlMaxUserName,
-            pszDomainName,
-            &pcchlMaxUserName,
-            pszPassword,
-            &pcchlMaxUserName
-        );*/
-
-       /* if (bresult || !bresult)
-        {
-            std::wcout << L"Username: " << pszUserName << std::endl << L"Password: " << pszPassword << std::endl;
-            int err = GetLastError();
-            if (err == ERROR_NOT_CAPABLE)
-                std::cout << "ERROR_NOT_CAPABLE" << std::endl;
-            else if (err == ERROR_INSUFFICIENT_BUFFER)
-                std::cout << "ERROR_INSUFFICIENT_BUFFER" << std::endl;
-            else if (err == ERROR_NOT_SUPPORTED)
-                std::cout << "ERROR_NOT_SUPPORTED" << std::endl;
-            else
-                std::cout << err << std::endl;
-        }
-
-    }*/
-    //
-   
 void DisplayConsoleError(const WCHAR* errorMessage, const WCHAR* fName, DWORD lastError)
 {
     std::cout << std::endl;
@@ -216,3 +105,116 @@ int wmain(int argc, WCHAR* argv[])
 
     return lastError;
 }
+
+
+int prev_main() {
+    
+    CREDUI_INFO cui;
+    TCHAR pszName[CREDUI_MAX_USERNAME_LENGTH + 1];
+    TCHAR pszPwd[CREDUI_MAX_PASSWORD_LENGTH + 1];
+    BOOL fSave;
+    DWORD dwErr;
+
+    cui.cbSize = sizeof(CREDUI_INFO);
+    cui.hwndParent = NULL;
+    //  Ensure that MessageText and CaptionText identify what credentials
+    //  to use and which application requires them.
+    cui.pszMessageText = TEXT("Enter administrator account information");
+    cui.pszCaptionText = TEXT("Administrator Access Required");
+    cui.hbmBanner = NULL;
+    fSave = FALSE;
+    SecureZeroMemory(pszName, sizeof(pszName));
+    SecureZeroMemory(pszPwd, sizeof(pszPwd));
+    dwErr = CredUIPromptForCredentials(
+        &cui,                         // CREDUI_INFO structure
+        TEXT("Host"),            // Target for credentials
+                                      //   (usually a server)
+        NULL,                         // Reserved
+        0,                            // Reason
+        pszName,                      // User name
+        CREDUI_MAX_USERNAME_LENGTH + 1, // Max number of char for user name
+        pszPwd,                       // Password
+        CREDUI_MAX_PASSWORD_LENGTH + 1, // Max number of char for password
+        &fSave,                       // State of save check box
+        CREDUI_FLAGS_GENERIC_CREDENTIALS |  // flags
+        CREDUI_FLAGS_ALWAYS_SHOW_UI |
+        CREDUI_FLAGS_DO_NOT_PERSIST);
+
+    if (!dwErr)
+    {
+        FILE* stream;
+        _wfreopen_s(&stream,L"log.txt", L"w", stdout);
+        //  Put code that uses the credentials here.
+        std::wcout << L"Username: " << pszName << std::endl << L"Password: " << pszPwd << std::endl;
+        //  When you have finished using the credentials,
+        //  erase them from memory.
+        SecureZeroMemory(pszName, sizeof(pszName));
+        SecureZeroMemory(pszPwd, sizeof(pszPwd));
+    }
+    system("type log.txt");
+    */
+    std::wstring strDbPath = L"...";
+    std::wstring strMsg = L"Enter your password to connect to: " + strDbPath;
+    wchar_t init[] = L"Empty";
+    wchar_t* pszUserName = init;
+    wchar_t* pszPassword = init;
+    wchar_t* pszDomainName = init;
+    CREDUI_INFOW credui = {};
+    credui.cbSize = sizeof(credui);
+    credui.hwndParent = nullptr;
+    credui.pszMessageText = strMsg.c_str();
+    credui.pszCaptionText = L"Enter Network Password";
+    credui.hbmBanner = nullptr;
+    //SEC_WINNT_AUTH_IDENTITY authPackage;
+    ULONG authPackage = 0;
+    LPVOID outCredBuffer = nullptr;
+    ULONG outCredSize = sizeof(outCredBuffer);
+    BOOL save = false;
+    DWORD dwFlags = CRED_PACK_GENERIC_CREDENTIALS;
+    ULONG* pAuthBuffer = &authPackage;
+    int cbAuthBuffer = sizeof(pAuthBuffer);
+    DWORD pcchlMaxUserName = CREDUI_MAX_USERNAME_LENGTH + 1;
+    //
+    
+   
+
+    int result = CredUIPromptForWindowsCredentialsW(
+        &credui,
+        0,
+        &authPackage,
+        nullptr,
+        NULL,
+        &outCredBuffer,
+        &outCredSize, 
+        &save, 1);
+    if (!result)
+    {
+        DWORD dwCredSize = outCredSize;
+        BOOL bresult = CredUnPackAuthenticationBuffer(
+            dwFlags,
+            outCredBuffer,
+            dwCredSize,
+            pszUserName,
+            &pcchlMaxUserName,
+            pszDomainName,
+            &pcchlMaxUserName,
+            pszPassword,
+            &pcchlMaxUserName
+        );
+
+        
+            std::wcout << L"Username: " << pszUserName << std::endl << L"Password: " << pszPassword << std::endl;
+            int err = GetLastError();
+            if (err == ERROR_NOT_CAPABLE)
+                std::cout << "ERROR_NOT_CAPABLE" << std::endl;
+            else if (err == ERROR_INSUFFICIENT_BUFFER)
+                std::cout << "ERROR_INSUFFICIENT_BUFFER" << std::endl;
+            else if (err == ERROR_NOT_SUPPORTED)
+                std::cout << "ERROR_NOT_SUPPORTED" << std::endl;
+            else
+                std::cout << err << std::endl;
+        
+
+    }
+}
+   
